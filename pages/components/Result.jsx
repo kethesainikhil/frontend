bff70ffcd30206c3db140fccc541184d4e348e27
kethesainikhil <nikhilkethe007@gmail.com>
2024-03-21 19:59:10 +0530

    animation added

--- pages/components/Result.jsx
+++ pages/components/Result.jsx
@@ -17,7 +17,7 @@ const Result = () => {
     if (loading) {
         return (
             <div className="flex flex-col items-center justify-center h-screen">
-                <svg className="animate-spin h-10 w-10 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <svg className="animate-spin h-10 w-10 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.002 8.002 0 014 12H0c0 6.627 5.373 12 12 12v-4c-3.86 0-7.278-1.492-9.899-3.899l2.828-2.828zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4c0 2.21-.898 4.209-2.343 5.657l-2.828-2.828A7.963 7.963 0 0020 12z"></path>
                 </svg>
